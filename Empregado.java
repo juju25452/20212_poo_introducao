5a8998114bc1f85ae8f3a1c1b94a5726c2bf0e36
Yago Siqueira <yago.yyaaggoo@gmail.com>
2021-08-05 16:21:08 -0300

    Adicionando commits faltantes do commit anterior

--- Empregado.java
+++ Empregado.java
@@ -1,3 +1,45 @@
 public class Empregado {
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double bonus;
 
+    public void setTipo (int Tipo);{
+        this.tipo = tipo;
+    }
+
+    public int getTipo(){
+        return this.tipo;
+    }
+
+
+    public double calculaSalario(){
+        if (tipo ==1 ){
+            return salario;
+        }
+        else if (tipo ==2){
+            return salario + salario * comissao;
+        }
+        else if (tipo ==3){
+            return salario + bonus;
+        }
+        return 0;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome (){
+        return this.nome;
+    }
+    
+    public void  setIdade (int idade){
+        this.idade = idade;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
 }
---
+++ TesteEmpregado.java
@@ -0,0 +1,8 @@
+public class TesteEmpregado{
+    public static void main (String){
+        Empregado e1 = new Empregado();
+        Empregado e2 = new Empregado();
+        Empregado e3 = new Empregado();
+
+    }
+}
